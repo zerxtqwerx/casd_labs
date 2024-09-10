46af84c69dbf2cffd23bf0afbb5b42b07655b8b3
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-09-10 23:38:42 +0300

    task 2

---
+++ ConsoleApp1/ConsoleApp1/ComplexDigit.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ComplexDigit
+    {
+        private double Real;
+        private double Imaginary;
+
+        public ComplexDigit(double Real = 0, double Imaginary = 0)
+        {
+            this.Real = Real;
+            this.Imaginary = Imaginary;
+        }
+
+        //возврат вещественной и мнимой частей
+        public double RealPart() { return Real; }
+        public double ImaginaryPart() { return Imaginary; }
+
+        //сложение
+        public ComplexDigit Add(ComplexDigit cd)
+        {
+            ComplexDigit answer = new ComplexDigit();
+            answer.Real = this.Real + cd.Real;
+            answer.Imaginary = this.Imaginary + cd.Imaginary;
+            return answer;
+        }
+
+        //умножение
+        public ComplexDigit Multiply(ComplexDigit cd)
+        {
+            ComplexDigit answer = new ComplexDigit();
+            answer.Real = this.Real * cd.Real - this.Imaginary * cd.Imaginary;
+            answer.Imaginary = this.Real * cd.Imaginary + this.Imaginary * cd.Real;
+            return answer;
+        }
+
+        //вычитание
+        public ComplexDigit Difference(ComplexDigit cd)
+        {
+            ComplexDigit answer = new ComplexDigit();
+            answer.Real = this.Real - cd.Real;
+            answer.Imaginary = this.Imaginary - cd.Imaginary;
+            return answer;
+        }
+
+        //деление
+        public ComplexDigit Divide(ComplexDigit cd)
+        {
+            ComplexDigit answer = new ComplexDigit();
+            answer.Real = (this.Real * cd.Real + this.Imaginary * cd.Imaginary) / (Math.Pow(cd.Real, 2) + Math.Pow(cd.Imaginary, 2));
+            answer.Imaginary = (this.Imaginary * cd.Real - this.Real * cd.Imaginary) / (Math.Pow(cd.Real, 2) + Math.Pow(cd.Imaginary, 2));
+            return answer;
+        }
+
+        //нахождение модуля
+        public double FindModule()
+        {
+            return Math.Sqrt(Math.Pow(this.Real, 2) + Math.Pow(this.Imaginary, 2));
+        }
+
+
+        //нахождения аргумента 
+        public double FindArgument()
+        {
+            if (this.Real > 0)
+            {
+                return Math.Tan(this.Imaginary / this.Real);
+            }
+            else if (this.Real < 0 && this.Imaginary >= 0)
+            {
+                return Math.Tan(this.Imaginary / this.Real) + Math.PI;
+            }
+            else if (this.Real < 0 && this.Imaginary < 0)
+            {
+                return Math.Tan(this.Imaginary / this.Real) - Math.PI;
+            }
+            else if (this.Real == 0 && this.Imaginary > 0)
+            {
+                return Math.PI / 2;
+            }
+            else if (this.Real == 0 && this.Imaginary < 0)
+            {
+                return Math.PI / (-2);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/task2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+/*Определить структуру для хранения комплексных чисел. Методы: создание комплексного
+числа по вещественной и мнимой части, сложение, вычитание, умножение, деление,
+нахождение модуля и аргумента, возврат мнимой и вещественной частей, вывод. При
+необходимости передавать структуру в качестве параметра используйте константные
+ссылки.
+Для демонстрации работы методов организуйте текстовое меню с помощью бесконечного
+цикла. Выбор действий организовать вводом символа, который обрабатывается
+оператором switch. Кроме методов необходимо иметь возможность выхода из программы
+по пункту меню. Выход может быть осуществлён по символам 'Q' и 'q'. При вводе
+неизвестного символа, должно быть выведено сообщение «неизвестная команда».
+(Операция ввода получает одно число, с которыми должны работать остальные операции.
+Если операции необходимо два аргумента – введите второй аргумент. Результат должен
+записываться в исходной число. До первого ввода считать число равным 0).*/
+
+
+namespace ConsoleApp1
+{
+    internal class task2
+    {
+        static void Main(string[] args)
+        {
+            int n = -1;
+            while (n != 0)
+            {
+                n = -1;
+                Console.WriteLine("Выберите одну из предложенных команд по порядковому номеру: ");
+                Console.WriteLine("1. Сложение");
+                Console.WriteLine("2. Вычитание");
+                Console.WriteLine("3. Умножение");
+                Console.WriteLine("4. Деление");
+                Console.WriteLine("5. Нахождение модуля");
+                Console.WriteLine("6. Нахождение аргумента");
+                Console.WriteLine("Нажмите Q для выхода.");
+
+                string command = Console.ReadLine();
+
+                if (command == "q" || command == "Q")
+                {
+                    n = 0;
+                }
+                else if(char.IsDigit(Convert.ToChar(command)))
+                {
+                    n = Convert.ToInt32(command);
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестная команда. повторите попытку.");
+                }
+                //если n - одна из команд
+                if (n != -1)
+                {
+                    if (n == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Введите комплексное число: ");
+                    ComplexDigit complex1 = new ComplexDigit();
+                    complex1 = InputComplex();
+
+                    ComplexDigit complex2 = new ComplexDigit();
+                    if (n >= 1 && n <= 4)
+                    {
+                        Console.WriteLine("Введите второе комплексное число: ");
+                        complex2 = InputComplex();
+                    }
+
+                    ComplexDigit answer = new ComplexDigit();
+                    Console.WriteLine("Ответ: ");
+                    switch (n)
+                    {
+                        case 1:
+                            answer = complex1.Add(complex2);
+                            Console.WriteLine(Convert.ToString(answer.RealPart()) + "+ i*" + Convert.ToString(answer.ImaginaryPart()));
+                            break;
+                        case 2:
+                            answer = complex1.Difference(complex2);
+                            Console.WriteLine(Convert.ToString(answer.RealPart()) + "+ i*" + Convert.ToString(answer.ImaginaryPart()));
+                            break;
+                        case 3:
+                            answer = complex1.Multiply(complex2);
+                            Console.WriteLine(Convert.ToString(answer.RealPart()) + "+ i*" + Convert.ToString(answer.ImaginaryPart()));
+                            break;
+                        case 4:
+                            answer = complex1.Divide(complex2);
+                            Console.WriteLine(Convert.ToString(answer.RealPart()) + "+ i*" + Convert.ToString(answer.ImaginaryPart()));
+                            break;
+                        case 5:
+                            Console.WriteLine(complex1.FindModule());
+                            break;
+                        case 6:
+                            Console.WriteLine(complex1.FindArgument());
+                            break;
+                        default:
+                            break;
+                    }
+                    Console.WriteLine("\t");
+                }
+            }
+        }
+
+        public static ComplexDigit InputComplex()
+        {
+            string r, i;
+            do
+            {
+                Console.WriteLine("Введите вещественную часть: ");
+                r = Console.ReadLine();
+
+                Console.WriteLine("Введите мнимую часть: ");
+                i = Console.ReadLine();
+            }
+            while (!char.IsDigit(Convert.ToChar(r)) || !char.IsDigit(Convert.ToChar(i)));
+            return new ComplexDigit(Convert.ToDouble(r), Convert.ToDouble(i));
+        }
+    }
+}
