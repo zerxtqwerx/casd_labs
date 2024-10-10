8487b8c4167156520abeba03911a794f6f1d3c0e
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-10-10 14:38:45 +0300

    add regular function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace ConsoleApp1
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string filePath = "input.txt";
+
+        HashSet<string> tags = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        try
         {
+            foreach (var line in File.ReadLines(filePath))
+            {
+                var matches = Regex.Matches(line, @"<\s*\/?\s*([a-zA-Z][a-zA-Z0-9]*)\s*>");
+                foreach (Match match in matches)
+                {
+                    string tag = match.Value.Trim('<', '>', ' ');
+
+                    tags.Add(match.Groups[1].Value.Trim());
+                }
+            }
+
+            foreach (var tag in tags)
+            {
+                Console.WriteLine(tag);
+            }
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            Console.ReadLine();
         }
     }
 }
