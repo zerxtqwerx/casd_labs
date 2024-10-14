1603ef36eed81f3a99495b299177635e5f31ef37
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-10-14 23:58:00 +0300

    create stack

---
+++ ConsoleApp1/ConsoleApp1/MyStack.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp1;
+using System.Collections.Generic;
+using System;
+
+public class MyStack<T> : MyVector<T>
+{
+    public void Push(T item)
+    {
+        Add(item);
+    }
+
+    public T Pop()
+    {
+        if (Empty())
+        {
+            throw new InvalidOperationException("Stack is Empty, cannot pop.");
+        }
+        return base.Remove(Size() - 1);
+    }
+
+    public T Peek()
+    {
+        if (Empty())
+        {
+            throw new InvalidOperationException("Stack is Empty, cannot peek.");
+        }
+
+        return Get(Size() - 1);
+    }
+
+    public bool Empty()
+    {
+        return Size() == 0;
+    }
+
+    public int Search(T item)
+    {
+        for (int i = 0; i < Size(); i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(Get(Size() - i - 1), item))
+            {
+                return i + 1; // Возвращаем глубину (позицию)
+            }
+        }
+
+        return -1; // Если элемент не найден
+    }
+}
