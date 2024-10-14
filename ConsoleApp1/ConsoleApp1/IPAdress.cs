6eab4bee3f78b4d018cfd3fcc877a02cc362fc2e
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-10-14 21:20:20 +0300

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/IPAdress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class IPAdress
+    {
+        //0.0.0.0
+        //255.255.255.255
+
+    }
+}
