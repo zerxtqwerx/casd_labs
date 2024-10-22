b81545361a90590d962711a88e349ec6751e761e
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-10-22 21:24:54 +0300

    fix

--- ConsoleApp1/ConsoleApp1/Heap.cs
+++ ConsoleApp1/ConsoleApp1/Heap.cs
@@ -4,43 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class MyPriorityQueue<T>
+public class Heap<T> where T : IComparable<T>
 {
-    private T[] queue;
+    private T[] elements;
     private int size;
-    private IComparer<T> comparator;
+    private readonly bool isMaxHeap;
 
     //1
-    public MyPriorityQueue() : this(11, null) { }
-
-    //2
-    public MyPriorityQueue(T[] a)
+    public Heap(T[] array, bool isMaxHeap = true)
     {
-        queue = new T[a.Length];
-        Array.Copy(a, queue, a.Length);
-        size = a.Length;
-        comparator = Comparer<T>.Default;
-        BuildHeap();
-    }
-
-    //3
-    public MyPriorityQueue(int initialCapacity) : this(initialCapacity, null) { }
-
-    //4
-    public MyPriorityQueue(int initialCapacity, IComparer<T> comparator)
-    {
-        this.queue = new T[initialCapacity];
-        this.size = 0;
-        this.comparator = comparator ?? Comparer<T>.Default;
-    }
-
-    //5
-    public MyPriorityQueue(MyPriorityQueue<T> c)
-    {
-        queue = new T[c.size];
-        Array.Copy(c.queue, queue, c.size);
-        size = c.size;
-        comparator = c.comparator;
+        this.isMaxHeap = isMaxHeap;
+        this.size = array.Length;
+        elements = new T[size];
+        Array.Copy(array, elements, size);
         BuildHeap();
     }
 
@@ -52,15 +28,87 @@ public class MyPriorityQueue<T>
         }
     }
 
+    //2
+    public T Peek()
+    {
+        if (size == 0)
+            throw new InvalidOperationException("Heap is empty.");
+        return elements[0];
+    }
+
+    //3
+    public T Extract()
+    {
+        if (size == 0)
+            throw new InvalidOperationException("Heap is empty.");
+
+        T root = elements[0];
+        elements[0] = elements[size - 1];
+        size--;
+        Heapify(0);
+        return root;
+    }
+
+    //4
+    public void IncreaseKey(int index, T newValue)
+    {
+        if (index < 0 || index >= size)
+            throw new ArgumentOutOfRangeException(nameof(index));
+
+        if ((isMaxHeap && newValue.CompareTo(elements[index]) < 0) ||
+            (!isMaxHeap && newValue.CompareTo(elements[index]) > 0))
+            throw new ArgumentException("New value is not applicable.");
+
+        elements[index] = newValue;
+
+        while (index > 0 && Compare(elements[Parent(index)], elements[index]) > 0)
+        {
+            Swap(index, Parent(index));
+            index = Parent(index);
+        }
+    }
+
+    //5
+    public void Add(T value)
+    {
+        if (size == elements.Length)
+        {
+            Array.Resize(ref elements, size * 2);
+        }
+
+        elements[size] = value;
+        size++;
+        int index = size - 1;
+
+        while (index > 0 && Compare(elements[Parent(index)], elements[index]) > 0)
+        {
+            Swap(index, Parent(index));
+            index = Parent(index);
+        }
+    }
+
+    //6
+    public Heap<T> Merge(Heap<T> other)
+    {
+        if (other == null)
+            throw new ArgumentNullException(nameof(other));
+
+        T[] mergedArray = new T[size + other.size];
+        Array.Copy(elements, mergedArray, size);
+        Array.Copy(other.elements, 0, mergedArray, size, other.size);
+        return new Heap<T>(mergedArray, isMaxHeap);
+    }
+
     private void Heapify(int index)
     {
         int left = LeftChild(index);
         int right = RightChild(index);
         int extreme = index;
 
-        if (left < size && Compare(queue[left], queue[extreme]) < 0)
+        if (left < size && Compare(elements[left], elements[extreme]) < 0)
             extreme = left;
-        if (right < size && Compare(queue[right], queue[extreme]) < 0)
+
+        if (right < size && Compare(elements[right], elements[extreme]) < 0)
             extreme = right;
 
         if (extreme != index)
@@ -70,197 +118,19 @@ public class MyPriorityQueue<T>
         }
     }
 
+    private int Parent(int index) => (index - 1) / 2;
     private int LeftChild(int index) => 2 * index + 1;
     private int RightChild(int index) => 2 * index + 2;
 
     private void Swap(int i, int j)
     {
-        T temp = queue[i];
-        queue[i] = queue[j];
-        queue[j] = temp;
+        T temp = elements[i];
+        elements[i] = elements[j];
+        elements[j] = temp;
     }
 
-    private int Compare(T x, T y) => comparator.Compare(x, y);
-
-    //6
-    public void Add(T e)
+    private int Compare(T x, T y)
     {
-        if (size >= queue.Length)
-            Resize();
-
-        queue[size] = e;
-        size++;
-        HeapifyUp(size - 1);
-    }
-
-    private void HeapifyUp(int index)
-    {
-        while (index > 0)
-        {
-            int parent = (index - 1) / 2;
-            if (Compare(queue[parent], queue[index]) <= 0) break;
-            Swap(parent, index);
-            index = parent;
-        }
-    }
-
-    private void Resize()
-    {
-        int newCapacity = queue.Length < 64 ? queue.Length + 2 : (int)(queue.Length * 1.5);
-        Array.Resize(ref queue, newCapacity);
-    }
-
-    //7
-    public void AddAll(T[] a)
-    {
-        foreach (var item in a)
-        {
-            Add(item);
-        }
-    }
-
-    //8
-    public void Clear()
-    {
-        queue = new T[queue.Length];
-        size = 0;
-    }
-
-    //9
-    public bool Contains(object o)
-    {
-        if (o is T item)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (queue[i].Equals(item))
-                    return true;
-            }
-        }
-        return false;
-    }
-
-    //10
-    public bool ContainsAll(T[] a)
-    {
-        foreach (var item in a)
-        {
-            if (!Contains(item)) return false;
-        }
-        return true;
-    }
-
-    //11
-    public bool IsEmpty() => size == 0;
-
-    //12
-    public bool Remove(object o)
-    {
-        if (o is T item)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                if (queue[i].Equals(item))
-                {
-                    RemoveAt(i);
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    private void RemoveAt(int index)
-    {
-        if (index < 0 || index >= size)
-            throw new ArgumentOutOfRangeException(nameof(index));
-
-        queue[index] = queue[size - 1];
-        size--;
-        Heapify(index);
-    }
-
-    public void RemoveAll(T[] a)
-    {
-        foreach (var item in a)
-        {
-            Remove(item);
-        }
-    }
-
-    //14
-    public void RetainAll(T[] a)
-    {
-        HashSet<T> toRetain = new HashSet<T>(a);
-        for (int i = 0; i < size; i++)
-        {
-            if (!toRetain.Contains(queue[i]))
-            {
-                RemoveAt(i);
-                i--; // Adjust index after removal
-            }
-        }
-    }
-
-    //15
-    public int Size() => size;
-
-    //16
-    public T[] ToArray()
-    {
-        T[] result = new T[size];
-        Array.Copy(queue, result, size);
-        return result;
-    }
-
-    //17
-    public T[] ToArray(T[] a)
-    {
-        if (a == null || a.Length < size)
-            a = new T[size];
-        Array.Copy(queue, a, size);
-        return a;
-    }
-
-    //18
-    public T Element()
-    {
-        if (IsEmpty())
-            throw new InvalidOperationException("Priority queue is empty.");
-        return queue[0];
-    }
-
-    //19
-    public bool Offer(T obj)
-    {
-        try
-        {
-            Add(obj);
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-
-    //20
-    public T Peek()
-    {
-        if (IsEmpty())
-            return default(T);
-        return queue[0];
-    }
-
-    //21
-    public T Poll()
-    {
-        if (IsEmpty())
-            return default(T);
-        T result = queue[0];
-        RemoveAt(0);
-        return result;
+        return isMaxHeap ? x.CompareTo(y) : y.CompareTo(x);
     }
 }
-
-
