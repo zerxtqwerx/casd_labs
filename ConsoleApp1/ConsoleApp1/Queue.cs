df24938aa76500e84cbf921e1ec774b7c069087f
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-10-24 11:55:42 +0300

    fix

--- ConsoleApp1/ConsoleApp1/Queue.cs
+++ ConsoleApp1/ConsoleApp1/Queue.cs
@@ -197,7 +197,7 @@ public class MyPriorityQueue<T>
             if (!toRetain.Contains(queue[i]))
             {
                 RemoveAt(i);
-                i--; -
+                i--; 
             }
         }
     }
