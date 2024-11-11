60551e6b7f04ba7b7cc3fda0e4d08a5971161c3b
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-11-11 19:28:28 +0300

    //

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -55,7 +55,7 @@ public class Program
                 i++;
             }
         }
-
+        //
         Console.WriteLine("записанная в двунаправленной очереди строка: ");
         while (!deque.IsEmpty())
         {
