40f3c6534611eb9375d9565c69b069d9c05adc93
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-11-19 15:20:05 +0300

    task 20

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
-namespace ConsoleApp1
+public enum VariableType
 {
-    internal class Program
+    Int,
+    Float,
+    Double
+}
+public class VariableDefinition
+{
+    public VariableType Type { get; set; }
+    public string Value { get; set; }
+
+    public VariableDefinition(VariableType type, string value)
     {
-        static void Main(string[] args)
+        Type = type;
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Type} => {Value}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        string path = "input.txt";
+        MyHashMap<string, VariableDefinition> variableMap = new MyHashMap<string, VariableDefinition>();
+
+        Regex regex = new Regex(@"^(int|float|double)\s+([a-zA-Z_][\w]*)\s*=\s*(\d+([.]\d+)?);?$", RegexOptions.Multiline);
+
+
+        string[] lines = File.ReadAllLines(path);
+        List<string> errorMessages = new List<string>();
+        HashSet<string> duplicates = new HashSet<string>();
+
+        foreach (var line in lines)
         {
+            Match match = regex.Match(line);
+            if (match.Success)
+            {
+                VariableType type;
+                string name = match.Groups[2].Value;
+                string value = match.Groups[3].Value;
+
+                switch (match.Groups[1].Value.ToLower())
+                {
+                    case "int":
+                        type = VariableType.Int;
+                        break;
+                    case "float":
+                        type = VariableType.Float;
+                        break;
+                    case "double":
+                        type = VariableType.Double;
+                        break;
+                    default:
+                        continue;
+                }
+
+
+                if (variableMap.ContainsKey(name))
+                {
+                    duplicates.Add(name);
+                }
+                else
+                {
+                    VariableDefinition variable = new VariableDefinition(type, value);
+                    variableMap.Put(name, variable);
+                }
+            }
+            else
+            {
+                errorMessages.Add($"Некорректное определение: {line}");
+            }
         }
+
+        StreamWriter writer = new StreamWriter("output.txt");
+
+        foreach (var entry in variableMap.EntrySet())
+        {
+            writer.WriteLine($"{entry.Value.Type} => {entry.Key}({entry.Value.Value})");
+        }
+
+        if (duplicates.Count > 0)
+        {
+            writer.WriteLine("\nПереопределенные переменные:");
+            foreach (string duplicate in duplicates)
+            {
+                writer.WriteLine(duplicate);
+            }
+        }
+
+        if (errorMessages.Count > 0)
+        {
+            writer.WriteLine("\nНекорректные определения:");
+            foreach (string error in errorMessages)
+            {
+                writer.WriteLine(error);
+            }
+        }
+        writer.Close();
+        Console.WriteLine("Данные записаны в файл: " + Directory.GetCurrentDirectory() + "\\output.txt");
+        Console.ReadLine();
+
     }
 }
