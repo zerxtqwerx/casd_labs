e00cf812091fca3f35fa223ff18ab23b449c36de
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-11-19 15:02:52 +0300

    fix

--- ConsoleApp1/ConsoleApp1/MyHashMap.cs
+++ ConsoleApp1/ConsoleApp1/MyHashMap.cs
@@ -1,221 +1,210 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Xml.Linq;
 
-namespace ConsoleApp1
+public class MyHashMap<K, V>
 {
-
-    internal class MyHashMap<K, V>
+    private class Entry
     {
-        private class Entry
-        {
-            public K Key { get; set; }
-            public V Value { get; set; }
-            public Entry Next { get; set; }
+        public K Key { get; set; }
+        public V Value { get; set; }
+        public Entry Next { get; set; }
 
-            public Entry(K key, V value)
-            {
-                Key = key;
-                Value = value;
-            }
+        public Entry(K key, V value)
+        {
+            Key = key;
+            Value = value;
+            Next = null;
         }
+    }
 
-        private int GetHashCode(K key)
+    private Entry[] table;
+    private int size;
+    private float loadFactor;
+    private int threshold;
+
+    public MyHashMap() : this(16, 0.75f) { }
+
+    public MyHashMap(int initialCapacity) : this(initialCapacity, 0.75f) { }
+
+    public MyHashMap(int initialCapacity, float loadFactor)
+    {
+        if (initialCapacity < 1) throw new ArgumentOutOfRangeException("initialCapacity");
+        if (loadFactor <= 0) throw new ArgumentOutOfRangeException("loadFactor");
+
+        this.loadFactor = loadFactor;
+        table = new Entry[initialCapacity];
+        threshold = (int)(initialCapacity * loadFactor);
+        size = 0;
+    }
+
+    public void Clear()
+    {
+        Array.Clear(table, 0, table.Length);
+        size = 0;
+    }
+
+
+    public bool ContainsKey(K key)
+    {
+        return GetEntry(key) != null;
+    }
+
+    public bool ContainsValue(V value)
+    {
+        foreach (var entry in table)
         {
-            return Math.Abs(key.GetHashCode()) % size;
-        }
-        private int GetHashCode(V value)
-        {
-            return Math.Abs(value.GetHashCode()) % size;
-        }
-
-        Entry[] table;
-        int size;
-        float loadFactor;
-
-        //1
-        public MyHashMap()
-        {
-            table = new Entry[16];
-            size = 16;
-            loadFactor = 0.75f;
-
-        }
-
-        //2
-        public MyHashMap(int initialCapacity)
-        {
-            table = new Entry[initialCapacity];
-            size = table.Length;
-            loadFactor = 0.75f;
-        }
-
-        //3
-        public MyHashMap(int initialCapacity, float loadFactor)
-        {
-            table = new Entry[initialCapacity];
-            size = table.Length;
-            loadFactor = 0.75f;
-        }
-
-        //4
-        public void Clear()
-        {
-            table = new Entry[16];
-            size = table.Length;
-            loadFactor = 0.75f;
-        }
-
-        //5
-        public bool ContainsKey(object key)
-        {
-            int index = GetHashCode((K)key);
-            Entry current = table[index];
-            while(current != null)
-            {
-                if((object)current.Key == key)
-                    return true;
-                current = current.Next;
-            }
-            return false;
-        }
-
-        //6
-        public bool ContainsValue(object value)
-        {
-            int index = GetHashCode((V)value);
-            Entry current = table[index];
+            var current = entry;
             while (current != null)
             {
-                if ((object)current.Value == value)
-                    return true;
+                if (current.Value.Equals(value)) return true;
                 current = current.Next;
             }
-            return false;
         }
+        return false;
+    }
 
-        //7
-        public HashSet<object> EntrySet()
+    public HashSet<KeyValuePair<K, V>> EntrySet()
+    {
+        var set = new HashSet<KeyValuePair<K, V>>();
+        foreach (var entry in table)
         {
-            HashSet<object> result = new HashSet<object>();
-            foreach (Entry entry in table)
-            {
-                Entry current = entry;
-                while (current != null)
-                {
-                    result.Add(current);
-                }
-            }
-            return result;
-        }
-
-        //8
-        public V Get(object key)
-        {
-            int index = GetHashCode((K)key);
-            Entry current = (Entry)table[index];
-
+            var current = entry;
             while (current != null)
             {
-                if ((object)current.Key == key)
-                {
-                    return current.Value;
-                }
+                set.Add(new KeyValuePair<K, V>(current.Key, current.Value));
                 current = current.Next;
             }
-
-            throw new KeyNotFoundException("Ключ не найден.");
         }
+        return set;
+    }
 
-        //9
-        public bool IsEmpty()
+    public V Get(K key)
+    {
+        var entry = GetEntry(key);
+        return entry == null ? default(V) : entry.Value;
+    }
+
+    public bool IsEmpty()
+    {
+        return size == 0;
+    }
+
+    public HashSet<K> KeySet()
+    {
+        var set = new HashSet<K>();
+        foreach (var entry in table)
         {
-            return size == 0;
-        }
-
-        //10
-        public HashSet<object> KeySet()
-        {
-            HashSet<object> result = new HashSet<object>();
-            foreach (Entry entry in table)
-            {
-                Entry current = entry;
-                while (current != null)
-                {
-                    result.Add(entry.Key);
-                    current = current.Next;
-                }
-            }
-            return result;
-        }
-
-        //11
-        public void Put(K key, V value)
-        {
-            int index = GetHashCode(key);
-            Entry current = (Entry)table[index];
-            int bucket = index % size;
-            Entry entry = table[bucket];
-
-            if (entry == null)
-            {
-                table[bucket] = new Entry(key, value);
-                size++;
-                return;
-            }
-
-            while (entry != null)
-            {
-                if (entry.Key.Equals(key))
-                {
-                    entry.Value = value;
-                    return;
-                }
-                if (entry.Next == null)
-                {
-                    break;
-                }
-                entry = entry.Next;
-            }
-            entry.Next = new Entry(key, value);
-            size++;
-        }
-
-        //12
-        public void Remove(object key)
-        {
-            int index = GetHashCode((K)key);
-
-            if (table[index] == null)
-                return;
-            if (table[index].Key.Equals(key))
-            {
-                table[index] = table[index].Next;
-                size--;
-                return;
-            }
-            Entry current = table[index];
-            Entry previous = null;
+            var current = entry;
             while (current != null)
             {
-                if (current.Key.Equals(key))
+                set.Add(current.Key);
+                current = current.Next;
+            }
+        }
+        return set;
+    }
+
+    public void Put(K key, V value)
+    {
+        if (size >= threshold)
+        {
+            Resize();
+        }
+
+        int index = GetBucketIndex(key);
+        Entry current = table[index];
+
+        while (current != null)
+        {
+            if (current.Key.Equals(key))
+            {
+                current.Value = value;
+                return;
+            }
+            current = current.Next;
+        }
+
+        Entry newEntry = new Entry(key, value);
+        newEntry.Next = table[index];
+        table[index] = newEntry;
+        size++;
+    }
+
+    public V Remove(K key)
+    {
+        int index = GetBucketIndex(key);
+        Entry current = table[index];
+        Entry previous = null;
+
+        while (current != null)
+        {
+            if (current.Key.Equals(key))
+            {
+                if (previous == null)
+                {
+                    table[index] = current.Next;
+                }
+                else
                 {
                     previous.Next = current.Next;
-                    size--;
-                    return;
                 }
+                size--;
+                return current.Value;
+            }
+            previous = current;
+            current = current.Next;
+        }
+        return default(V);
+    }
 
-                previous = current;
-                current = current.Next;
+    public int Size()
+    {
+        return size;
+    }
+
+    private int GetBucketIndex(K key)
+    {
+        int hashCode = key.GetHashCode();
+        return (hashCode & 0x7FFFFFFF) % table.Length;
+    }
+
+    private Entry GetEntry(K key)
+    {
+        int index = GetBucketIndex(key);
+        Entry current = table[index];
+
+        while (current != null)
+        {
+            if (current.Key.Equals(key))
+            {
+                return current;
+            }
+            current = current.Next;
+        }
+        return null;
+    }
+
+    private void Resize()
+    {
+        int newCapacity = table.Length * 2;
+        Entry[] newTable = new Entry[newCapacity];
+
+        foreach (var entry in table)
+        {
+            var current = entry;
+            while (current != null)
+            {
+                int newIndex = (current.Key.GetHashCode() & 0x7FFFFFFF) % newCapacity;
+                Entry next = current.Next;
+
+                current.Next = newTable[newIndex];
+                newTable[newIndex] = current;
+
+                current = next;
             }
         }
 
-        //13
-        public int Size()
-        {
-            return size;
-        }
+        table = newTable;
+        threshold = (int)(newCapacity * loadFactor);
     }
 }
