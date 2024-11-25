ece7c0c040fbb98cb15b73f2650a1f75e14328ce
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-11-25 20:23:44 +0300

    fixed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -35,6 +35,102 @@ class Program
         Console.OutputEncoding = Encoding.GetEncoding(1251);
         string path = "input.txt";
         MyHashMap<string, VariableDefinition> variableMap = new MyHashMap<string, VariableDefinition>();
+        Regex regex = new Regex(@"^(int|float|double)\s+([a-zA-Z_][\w]*)\s*=\s*([\d]+([.][\d]+)?);?$", RegexOptions.Multiline);
+
+        string[] lines = File.ReadAllLines(path);
+        List<string> errorMessages = new List<string>();
+        HashSet<string> duplicates = new HashSet<string>();
+
+        foreach (var line in lines)
+        {
+            Match match = regex.Match(line);
+            if (match.Success)
+            {
+                VariableType type;
+                string name = match.Groups[2].Value;
+                //string value = match.Groups[3].Value.Replace(',', '.'); // Заменяем запятую на точку
+
+                switch (match.Groups[1].Value.ToLower())
+                {
+                    case "int":
+                        type = VariableType.Int;
+                        break;
+                    case "float":
+                        type = VariableType.Float;
+                        break;
+                    case "double":
+                        type = VariableType.Double;
+                        break;
+                    default:
+                        continue;
+                }
+
+                // Проверяем на существование переменной и фиксируем дубликат
+                if (variableMap.ContainsKey(name))
+                {
+                    duplicates.Add(name); // добавляем имя переменной в дубликаты
+                }
+                else
+                {
+                    // Сохраняем новое определение переменной
+                    VariableDefinition variable = new VariableDefinition(type, match.Groups[3].Value);
+                    variableMap.Put(name, variable);
+                }
+            }
+            else
+            {
+                try
+                {
+                    string[] s = line.Split(' ');
+                    if (variableMap.ContainsKey((string)s[1]))
+                    {
+                        duplicates.Add(line);
+                    }
+                    else
+                    {
+                        errorMessages.Add(line);
+                    }
+                }
+                catch { errorMessages.Add(line); } 
+            }
+        }
+
+        // Запись результатов в файл
+        using (StreamWriter writer = new StreamWriter("output.txt"))
+        {
+            foreach (var entry in variableMap.EntrySet())
+            {
+                writer.WriteLine($"{entry.Value.Type} => {entry.Key}({entry.Value.Value})");
+            }
+
+            if (duplicates.Count > 0)
+            {
+                writer.WriteLine("\nПереопределенные переменные:");
+                foreach (string duplicate in duplicates)
+                {
+                    writer.WriteLine(duplicate);
+                }
+            }
+
+            if (errorMessages.Count > 0)
+            {
+                writer.WriteLine("\nНекорректные определения:");
+                foreach (string error in errorMessages)
+                {
+                    writer.WriteLine(error);
+                }
+            }
+        }
+
+        Console.WriteLine("Данные записаны в файл: " + Path.Combine(Directory.GetCurrentDirectory(), "output.txt"));
+        Console.Read();
+    }
+}
+    /*static void Main()
+    {
+        Console.OutputEncoding = Encoding.GetEncoding(1251);
+        string path = "input.txt";
+        MyHashMap<string, VariableDefinition> variableMap = new MyHashMap<string, VariableDefinition>();
 
         Regex regex = new Regex(@"^(int|float|double)\s+([a-zA-Z_][\w]*)\s*=\s*(\d+([.]\d+)?);?$", RegexOptions.Multiline);
 
@@ -83,6 +179,7 @@ class Program
                 Regex regex1 = new Regex(@"^(int|float|double)\s+([a-zA-Z_][\w]*)\s*=\s*(\d+([.]\d+)?);?$", RegexOptions.Multiline);
                 Match match1 = regex1.Match(line);
                 string name1 = match.Groups[2].Value;
+
                 if (match1.Success)
                 {
                     duplicates.Add(name1);
@@ -120,5 +217,4 @@ class Program
         Console.WriteLine("Данные записаны в файл: " + Directory.GetCurrentDirectory() + "\\output.txt");
         Console.ReadLine();
 
-    }
-}
+    }*/
