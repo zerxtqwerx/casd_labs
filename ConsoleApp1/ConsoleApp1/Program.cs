cf5f20381533e3c9052264bd584f6824b22713c2
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-12-09 20:29:51 +0300

    task 25

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            MyHashSet<string> linesSet = new MyHashSet<string>();
+            string[] lines = File.ReadAllLines("input.txt");
+
+            foreach (string line in lines)
+            {
+                linesSet.Add(line);
+            }
+
+            var sortedLines = linesSet.ToArray().OrderBy(x => x, new WordLengthComparer()).ToList();
+
+            foreach (var line in sortedLines)
+            {
+                Console.WriteLine(line);
+            }
+            Console.Read();
         }
     }
 }
