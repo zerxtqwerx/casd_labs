d63e8afd53ef2f926e522ca5be236bcd43f3526f
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-12-10 19:27:19 +0300

    task 26

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
+            MyHashSet<string> uniqueWords = new MyHashSet<string>();
+
+            string filePath = "input.txt";
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                MatchCollection matches = Regex.Matches(line.ToLower(), @"\b[a-zA-Z]+\b", RegexOptions.IgnoreCase);
+
+                foreach (Match match in matches)
+                {
+                    if(!uniqueWords.Contains(match.Value)) 
+                        uniqueWords.Add(match.Value);
+                }
+            }
+
+            Console.WriteLine("Уникальные слова (без учета регистра):");
+            foreach (var word in uniqueWords.ToArray().OrderBy(x => x).ToList()) 
+            {
+                Console.WriteLine(word);
+            }
+            Console.Read();
         }
     }
 }
---
+++ ConsoleApp1/ConsoleApp1/StringComparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class StringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            if (x == null && y == null) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            var wordLengthsX = GetWordLengths(x);
+            var wordLengthsY = GetWordLengths(y);
+
+            for (int i = 0; i < Math.Min(wordLengthsX.Count, wordLengthsY.Count); i++)
+            {
+                if (wordLengthsX[i] < wordLengthsY[i])
+                    return -1;
+                if (wordLengthsX[i] > wordLengthsY[i])
+                    return 1;
+            }
+
+            // Если длины равны до конца одной строки, считается, что более короткая строка меньше
+            return wordLengthsX.Count.CompareTo(wordLengthsY.Count);
+        }
+
+        private List<int> GetWordLengths(string line)
+        {
+            return line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                       .Select(word => word.Length)
+                       .OrderBy(len => len)
+                       .ToList();
+        }
+    }
+}
