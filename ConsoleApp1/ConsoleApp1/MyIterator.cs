2b73fe2085f8840f5aaa3d46703e3c0a054e1783
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-12-10 19:56:28 +0300

    update

---
+++ ConsoleApp1/ConsoleApp1/MyIterator.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    public interface MyIterator<T>
+    {
+        bool HasNext();
+        bool HasPrevious();
+
+        T Next();
+        T Previous();
+
+        T NextIndex();
+        T PreviousIndex();
+
+        void Remove();
+
+        void Set(T element);
+        void Add(T element);
+    }
+
+    public class MyItr<T> : MyIterator<T>
+    {
+        private T Cursor;
+
+        public bool HasNext()
+        {
+           
+        }
+    }
+}
