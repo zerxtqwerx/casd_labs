5be71ae98279146983a13e6c6248ac677e765c93
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-12-18 06:25:56 +0300

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int N = 6;
+            Console.WriteLine(((N - 1) % 9) + 1); //6 Топологическая сортировка. Алгоритм Тарьяна.
+            Console.WriteLine(((N - 1) % 3) + 10); //12 Построение максимального потока в транспортной сети. Алгоритм проталкивания предпотока
+            Console.WriteLine(((N - 1) % 6) + 13); //18 Проверка изоморфности двух графов.
+            Console.Read();
         }
     }
 }
