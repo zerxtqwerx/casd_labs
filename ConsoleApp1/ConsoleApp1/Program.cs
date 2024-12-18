f77b32a35b266d9ec1876f98f5cf07b7642a3e61
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-12-18 07:06:54 +0300

    add encoding

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.GetEncoding(1251);
             int N = 6;
             Console.WriteLine(((N - 1) % 9) + 1); //6 Топологическая сортировка. Алгоритм Тарьяна.
             Console.WriteLine(((N - 1) % 3) + 10); //12 Построение максимального потока в транспортной сети. Алгоритм проталкивания предпотока
