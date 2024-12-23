4643dccec6b99dd86e803078ad06fc1e15caedc4
zerxtqwerx <78277090+zerxtqwerx@users.noreply.github.com>
2024-12-23 22:59:15 +0300

    //

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        //
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.GetEncoding(1251);
